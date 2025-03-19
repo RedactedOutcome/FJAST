855cecbf5a8fdd542f0b3a63f30eabc80bb3d4b5
1WeirdDev <the1weirddev@gmail.com>
2025-03-19 13:50:08 -0500

    String Literals and fixed token move semantics

--- src/Token.cpp
+++ src/Token.cpp
@@ -2,12 +2,12 @@
 #include "Token.h"
 
 namespace FJASTP{
-    Token::Token(TokenType type, const HBuffer& value, size_t lineNumber, size_t columnNumber) noexcept
+    Token::Token(TokenType type, const HBuffer& value, uint32_t lineNumber, uint32_t columnNumber) noexcept
         : m_Type(type), m_Value(value), m_Metadata(0), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
-    Token::Token(TokenType type, const HBuffer& value, uint8_t metadata, size_t lineNumber, size_t columnNumber) noexcept
+    Token::Token(TokenType type, const HBuffer& value, uint8_t metadata, uint32_t lineNumber, uint32_t columnNumber) noexcept
         : m_Type(type), m_Value(value), m_Metadata(metadata), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
-    Token::Token(TokenType type, HBuffer&& value, size_t lineNumber, size_t columnNumber) noexcept
-        : m_Type(type), m_Value(value), m_Metadata(0), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
-    Token::Token(TokenType type, HBuffer&& value, uint8_t metadata, size_t lineNumber, size_t columnNumber) noexcept
-        : m_Type(type), m_Value(value), m_Metadata(metadata), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
+    Token::Token(TokenType type, HBuffer&& value, uint32_t lineNumber, uint32_t columnNumber) noexcept
+        : m_Type(type), m_Value(std::move(value)), m_Metadata(0), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
+    Token::Token(TokenType type, HBuffer&& value, uint8_t metadata, uint32_t lineNumber, uint32_t columnNumber) noexcept
+        : m_Type(type), m_Value(std::move(value)), m_Metadata(metadata), m_LineNumber(lineNumber), m_ColumnNumber(columnNumber){}
 }
